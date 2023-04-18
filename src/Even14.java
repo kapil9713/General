e75dd15b904aa3bef922d925f9f30eb48c085cba
Kapil Rathore <kapil9713@gmail.com>
2023-04-18 11:02:09 +0530

    even 14 created

---
+++ src/Even14.java
@@ -0,0 +1,23 @@
+
+public class Even14 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++) {
+			
+			if(i%2==0) {
+				System.out.println("Even="+i);
+			}else {
+				System.out.println("odd="+i);
+			}
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
