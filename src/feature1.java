2091ea3c009cbf27de3846470cf26b45239ddcf0
Kapil Rathore <kapil9713@gmail.com>
2023-04-18 19:49:47 +0530

    feature file add

--- src/feature1.java
+++ src/feature1.java
@@ -1,8 +1,12 @@
 
-public class feature1 {
+public class feature1{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("feature2");
+		
+		
+		
 
 	}
 
